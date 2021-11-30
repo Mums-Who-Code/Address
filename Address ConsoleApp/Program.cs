336fa245cd51a588ce515a62e62d32b7fe3d2569
Gracelin Jenifa <gracelinjenifa.k@outlook.com>
2021-11-30 23:05:54 +0530

    CONFIG: Initialize Console App

---
+++ Address ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+
+namespace Address_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
